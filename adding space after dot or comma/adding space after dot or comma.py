6c66dfb6776169fb09c0e22997b5f00edacdf987
Kahraman Kostas <36055021+bozbil@users.noreply.github.com>
2020-05-29 20:10:45 +0300

    Add files via upload

---
+++ adding space after dot or comma/adding space after dot or comma.py
@@ -0,0 +1,20 @@
+﻿import re
+ths = open("fixed.tex", "w")
+with open("simple.tex", "r") as file:
+    while True:
+        try:
+            line=file.readline()
+            if line=="":break
+            if (re.findall("\.[A-Z]",line))!=[]:   
+                print(error_list)
+                print(line)
+                error_list=re.findall("\.[A-Z]",line)
+                
+                for i in error_list:
+                    temp=i
+                    temp=temp.replace(".", ". ")
+                    line=line.replace(i,temp)
+            ths.write(line)
+        except:
+            continue
+ths.close()
