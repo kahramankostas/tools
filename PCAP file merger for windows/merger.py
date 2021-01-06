f9268e1aa0964cced875ccb78bf822e52a90d7de
Kahraman Kostas <36055021+kahramankostas@users.noreply.github.com>
2021-01-06 15:35:37 +0000

    Add files via upload

---
+++ PCAP file merger for windows/merger.py
@@ -0,0 +1,21 @@
+import os
+def find_the_way(path,file_format):
+    files_add = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if file_format in file:
+                files_add.append(os.path.join(r, file))  
+    return files_add
+files_add=find_the_way('./pcaps/','.pcap')
+files_add=str(files_add)
+files_add=files_add.replace("[","")
+files_add=files_add.replace("]","")
+files_add=files_add.replace("\'","")
+files_add=files_add.replace(",","")
+print(files_add)
+
+command="mergecap -w outfile.pcap "+files_add
+os.system(command)
+
+
