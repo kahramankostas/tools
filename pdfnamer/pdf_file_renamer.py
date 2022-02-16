ba2197ee4d722822ec7d37a5ae46d1551f4847b1
Kahraman Kostas <36055021+kahramankostas@users.noreply.github.com>
2022-02-16 12:24:29 +0000

    Update pdf_file_renamer.py

--- pdfnamer/pdf_file_renamer.py
+++ pdfnamer/pdf_file_renamer.py
@@ -1,7 +1,32 @@
-# Assigns the title of PDF files to the file name
+
+# coding: utf-8
+
+# # Assigns the title of PDF files to the file name
+
+# In[1]:
+
+
 import tika
 from tika import parser
 import os
+
+
+# In[2]:
+
+
+char=["<",
+">",
+":",
+"\"",
+"/",
+"\\",
+"|",
+"?",
+"*"]
+
+
+# In[4]:
+
 
 def find_the_way(path,file_format):
     files_add = []
@@ -16,7 +41,12 @@ files_add=find_the_way('./',"pdf")
 
 for i,ii in enumerate(files_add):
     FileName = ii
-    PDF_Parse = parser.from_file(FileName)    
-    new_name=str(PDF_Parse ['metadata']['dc:title'])+".pdf"
-    os.rename(ii,new_name) 
-
+    PDF_Parse = parser.from_file(FileName)   
+    try:
+        new_name=str(PDF_Parse ['metadata']['dc:title'])+".pdf"
+        for j in char:
+            if j in new_name:
+                new_name=new_name.replace(j," ")
+        os.rename(ii,new_name) 
+    except:
+        print(f"error:   {ii}")
