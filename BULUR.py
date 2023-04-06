9d28b0bd192f30d9ed648f0df8fb7970849acb82
Kahraman Kostas <36055021+kahramankostas@users.noreply.github.com>
2023-04-06 01:42:19 +0100

    Add files via upload

---
+++ BULUR.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import pandas as pd
+from tqdm import tqdm
+
+
+# In[3]:
+
+
+import os
+def find_the_way(path,file_format):
+    files_add = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if file_format in file:
+                files_add.append(os.path.join(r, file))  
+    return files_add
+files_add=find_the_way('./','.ipynb')
+files_add
+
+
+# In[4]:
+
+
+
+
+
+# In[6]:
+
+
+ara1="tshark"
+for i in (files_add):
+    try:
+        with open(i, "r", encoding="utf8") as file:
+      
+            line=file.read()
+            if ara1 in line:
+                print(i)
+    except:pass
+
+files_add=find_the_way('./','.py')
+files_add
+
+
+# In[4]:
+
+
+from tqdm import tqdm
+
+
+# In[6]:
+
+
+ara1="tshark"
+
+for i in (files_add):
+    try:
+        with open(i, "r", encoding="utf8") as file:
+      
+            line=file.read()
+            if ara1 in line:
+                print(i)
+    except:pass
+
+
+    
+
